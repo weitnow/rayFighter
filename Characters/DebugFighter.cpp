ea3d1fe8fd7cc1a6f7e11d0c1461222e5b47ee8c
Christian Keusch <weitnow@gmail.com>
2025-08-12 15:24:00 +0200

    debugFighter implemented

--- Characters/DebugFighter.cpp
+++ Characters/DebugFighter.cpp
@@ -26,35 +26,35 @@ void DebugFighter::init()
 
     setObjName("Andy Hug");
 
-    // [0, 1, 2...] = frameNumber, -1 = valid for all frames of gbFigher (ex. gbFighter-*), -2 = valid for all frames of gbFighter-Action (ex. gbFighter-Idle)
-    addCollisionBoxForFrame("gbFighter-Idle", -1, CollisionBoxType::PUSHBOX, 10, 0, 10, 30);
+    // [0, 1, 2...] = frameNumber, -1 = valid for all frames of gbFigher (ex. debugFighter-*), -2 = valid for all frames of debugFighter-Action (ex. debugFighter-Idle)
+    addCollisionBoxForFrame("debugFighter-Idle", -1, CollisionBoxType::PUSHBOX, 10, 0, 10, 30);
 
-    addCollisionBoxForFrame("gbFighter-Punch", -2, CollisionBoxType::PROXIMITYBOX, 16, 0, 40, 32);
-    addCollisionBoxForFrame("gbFighter-Punch", 1, CollisionBoxType::HITBOX, 31, 10, 5, 5);
+    addCollisionBoxForFrame("debugFighter-Punch", -2, CollisionBoxType::PROXIMITYBOX, 16, 0, 40, 32);
+    addCollisionBoxForFrame("debugFighter-Punch", 1, CollisionBoxType::HITBOX, 31, 10, 5, 5);
 
-    addCollisionBoxForFrame("gbFighter-Kick", -2, CollisionBoxType::PROXIMITYBOX, 16, 0, 40, 32);
-    addCollisionBoxForFrame("gbFighter-Kick", 1, CollisionBoxType::HITBOX, 26, 10, 5, 5);
+    addCollisionBoxForFrame("debugFighter-Kick", -2, CollisionBoxType::PROXIMITYBOX, 16, 0, 40, 32);
+    addCollisionBoxForFrame("debugFighter-Kick", 1, CollisionBoxType::HITBOX, 26, 10, 5, 5);
 
-    addCollisionBoxForFrame("gbFighter-DuckKick", -2, CollisionBoxType::PROXIMITYBOX, 16, 0, 40, 32);
-    addCollisionBoxForFrame("gbFighter-DuckKick", 1, CollisionBoxType::HITBOX, 26, 26, 5, 5);
+    addCollisionBoxForFrame("debugFighter-DuckKick", -2, CollisionBoxType::PROXIMITYBOX, 16, 0, 40, 32);
+    addCollisionBoxForFrame("debugFighter-DuckKick", 1, CollisionBoxType::HITBOX, 26, 26, 5, 5);
 
-    addCollisionBoxForFrame("gbFighter-DuckPunch", -2, CollisionBoxType::PROXIMITYBOX, 16, 0, 40, 32);
-    addCollisionBoxForFrame("gbFighter-DuckPunch", 0, CollisionBoxType::HITBOX, 30, 14, 5, 5);
+    addCollisionBoxForFrame("debugFighter-DuckPunch", -2, CollisionBoxType::PROXIMITYBOX, 16, 0, 40, 32);
+    addCollisionBoxForFrame("debugFighter-DuckPunch", 0, CollisionBoxType::HITBOX, 30, 14, 5, 5);
 
-    addCollisionBoxForFrame("gbFighter-JumpPunch", -2, CollisionBoxType::PROXIMITYBOX, 16, 0, 40, 32);
-    addCollisionBoxForFrame("gbFighter-JumpPunch", 0, CollisionBoxType::HITBOX, 24, 20, 5, 5);
+    addCollisionBoxForFrame("debugFighter-JumpPunch", -2, CollisionBoxType::PROXIMITYBOX, 16, 0, 40, 32);
+    addCollisionBoxForFrame("debugFighter-JumpPunch", 0, CollisionBoxType::HITBOX, 24, 20, 5, 5);
 
     // special moves
-    addCollisionBoxForFrame("gbFighter-A+B Move", -2, CollisionBoxType::PROXIMITYBOX, 16, 0, 40, 32);
-    addCollisionBoxForFrame("gbFighter-A+B Move", -2, CollisionBoxType::HITBOX, 0, 0, 35, 30);
+    addCollisionBoxForFrame("debugFighter-A+B Move", -2, CollisionBoxType::PROXIMITYBOX, 16, 0, 40, 32);
+    addCollisionBoxForFrame("debugFighter-A+B Move", -2, CollisionBoxType::HITBOX, 0, 0, 35, 30);
 
     // x, y, width, height
-    addCollisionBoxForFrame("gbFighter-Idle", -1, CollisionBoxType::HURTBOX, 10, 3, 12, 20, HurtboxType::HIGH);
-    addCollisionBoxForFrame("gbFighter-Idle", -1, CollisionBoxType::HURTBOX, 10, 23, 12, 8, HurtboxType::LOW);
+    addCollisionBoxForFrame("debugFighter-Idle", -1, CollisionBoxType::HURTBOX, 10, 3, 12, 20, HurtboxType::HIGH);
+    addCollisionBoxForFrame("debugFighter-Idle", -1, CollisionBoxType::HURTBOX, 10, 23, 12, 8, HurtboxType::LOW);
 
-    //addCollisionBoxForFrame("gbFighter-Idle", -1, CollisionBoxType::THROWBOX, HurtboxType::HIGH, true, 16, 10, 12, 20);
+    //addCollisionBoxForFrame("debugFighter-Idle", -1, CollisionBoxType::THROWBOX, HurtboxType::HIGH, true, 16, 10, 12, 20);
 
-    addCollisionBoxForFrame("gbFighter-Idle",
+    addCollisionBoxForFrame("debugFighter-Idle",
                             -1,
                             CollisionBoxType::THROWABLEBOX,
                             16,
